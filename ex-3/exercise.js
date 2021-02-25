b7eeef1f94fec5c5a36f55c0663aaf530297d454
Mohand Abboud <mohandabboud9@gmail.com>
2021-02-25 12:28:28 +0100

    solving ex-3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,20 @@
 // Example
-window.alert("Hello World!");
+
+var x = prompt("Wirte the first number");
+var y = prompt("Wirte the second number");
+
+if (x>y) {
+    console.log( "The greater number of " + x +" and " + y +" is " + x + ".");
+    window.alert("The greater number of " + x +" and " + y +" is " + x + ".");
+}
+
+else if (x<y) {
+    console.log( "The greater number of " + x +" and " + y +" is " + y + ".");
+    window.alert("The greater number of " + x +" and " + y +" is " + y + ".");
+}
+
+else {
+    console.log( "The numbers are the same.");
+    window.alert("The numbers are the same.");
+}
+ 
