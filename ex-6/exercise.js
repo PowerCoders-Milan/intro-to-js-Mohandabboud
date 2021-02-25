0932f8908941311c6417ca921bccc91a7c702f61
Mohand Abboud <mohandabboud9@gmail.com>
2021-02-25 13:14:28 +0100

    solving ex-6

--- ex-6/exercise.js
+++ ex-6/exercise.js
@@ -1,2 +1,7 @@
 // Example
-window.alert("Hello World!");
+
+var x = prompt("Please tell me a quotation");
+var y = prompt("can you also tell me what the author name?");
+
+window.alert( y + " " + "says, " + x + " .");
+
