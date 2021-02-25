5813662e7063eac262592cd06b0797b10b41bfa4
Mohand Abboud <mohandabboud9@gmail.com>
2021-02-25 12:10:28 +0100

    solving ex-2

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,2 +1,22 @@
 // Example
-window.alert("Hello World!");
+
+var born_year = prompt("Wirte the year you were born at");
+var furure_year = prompt("Wirte the year you want to know about");
+
+var age = furure_year - born_year ;
+var age_2 = age -1 ;
+
+window.alert("I will be either " + age_2 +" or " + age + " in " +  furure_year + ".");
+
+console.log("born_year" + " furure_year ");
+
+// Extra
+
+// var born_month = prompt("Wirte the number of the month you were born in");
+// var born_day = prompt("Wirte the number of the day you were born on");
+// var born_year_1 = prompt("Wirte the year you were born at");
+// var month_now = prompt("Wirte the number of the month you were born in");
+// var day_now = prompt("Wirte the number of the day you were born on");
+// var year_now = prompt("Wirte the year you were born at");
+// var year = year_now - born_year_1 ;
+// var month = month_now - 
