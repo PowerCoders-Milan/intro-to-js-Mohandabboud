bf0e60c80e066ba1d83cc02dc6d7f73b92a0ed68
Mohand Abboud <mohandabboud9@gmail.com>
2021-02-25 16:47:29 +0100

    solving ex-5 with the extra

--- ex-5/exercise.js
+++ ex-5/exercise.js
@@ -3,3 +3,17 @@
 var x = prompt("Wirte here anything you want");
 var y = x.length;
 window.alert( x + " " + y);
+
+var a = prompt("Wirte another phrase");
+var b = a.length;
+
+if (b < y) {
+    window.alert( x +" phrase was the longest with " + y +" number of characters."); 
+}
+else if (b > y) {
+    window.alert( a +" phrase was the longest with " + b +" number of characters."); 
+}
+
+else {
+    window.alert(" both phrases have the same number of characters which is " + y +".");
+}
